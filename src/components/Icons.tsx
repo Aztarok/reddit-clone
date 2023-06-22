b04468f4308c6bef8c007c0093daab1fe0ec0969
Aztarok <Aztarok2994@gmail.com>
2023-06-21 23:43:17 -0400

    Started on User Profile

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,6 +1,7 @@
-import { LucideIcon, LucideProps } from "lucide-react";
+import { LucideIcon, LucideProps, User } from "lucide-react";
 
 export const Icons = {
+    user: User,
     logo: (props: LucideProps) => (
         <svg {...props} viewBox="0 0 497 497">
             <g>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async () => {
     const session = await getAuthSession();
     return (
         <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
-            <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+            <div className="container min-w-7xl h-full mx-auto flex items-center justify-between gap-2">
                 <Link href="/" className="flex gap-2 items-center">
                     <Icons.logo className="h-8 w-8 sm:h-6 sm:w-6" />
                     <p className="hidden text-zinc-700 text-sm font-medium md:block">
@@ -15,8 +16,8 @@ const Navbar = async () => {
                     </p>
                 </Link>
 
-                {session ? (
-                    <p>You&apos;re loggged in</p>
+                {session?.user ? (
+                    <UserAccountNav user={session.user} />
                 ) : (
                     <Link href="/sign-in" className={buttonVariants()}>
                         Sign In
