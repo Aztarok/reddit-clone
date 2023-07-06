5ee261a894400ff0f155d5b00d10edc8c4c6cb4c
Aztarok <Aztarok2994@gmail.com>
2023-07-06 16:04:48 -0400

    Can reply and vote for comments

--- src/components/CommentsSection.tsx
+++ src/components/CommentsSection.tsx
@@ -51,8 +51,49 @@ const CommentsSection = async ({ postId }: CommentsSectionProps) => {
                                 className="flex flex-col"
                             >
                                 <div className="mb-2">
-                                    <PostComment comment={topLevelComment} />
+                                    <PostComment
+                                        postId={postId}
+                                        votesAmount={topLevelCommentVotesAmount}
+                                        currentVote={topLevelCommentVote}
+                                        comment={topLevelComment}
+                                    />
                                 </div>
+
+                                {/* Render replies */}
+                                {topLevelComment.replies
+                                    .sort(
+                                        (a, b) =>
+                                            b.votes.length - a.votes.length
+                                    )
+                                    .map((reply) => {
+                                        const replyVotesAmount =
+                                            reply.votes.reduce((acc, vote) => {
+                                                if (vote.type === "UP")
+                                                    return acc + 1;
+                                                if (vote.type === "DOWN")
+                                                    return acc + 1;
+                                                return acc;
+                                            }, 0);
+                                        const replyVote = reply.votes.find(
+                                            (vote) =>
+                                                vote.userId === session?.user.id
+                                        );
+                                        return (
+                                            <div
+                                                key={reply.id}
+                                                className="ml-2 py-2 pl-4 border-l-2 border-zinc-200"
+                                            >
+                                                <PostComment
+                                                    comment={reply}
+                                                    currentVote={replyVote}
+                                                    votesAmount={
+                                                        replyVotesAmount
+                                                    }
+                                                    postId={postId}
+                                                />
+                                            </div>
+                                        );
+                                    })}
                             </div>
                         );
                     })}
